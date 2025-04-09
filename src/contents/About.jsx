28ca28b379ccfaad72a8423946eac09adf7b9565
Reece Lardy <35311549+rlardy@users.noreply.github.com>
2025-04-09 03:09:52 -0500

    first commit

---
+++ src/contents/About.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+                <h1 className="subtopic">About Me</h1>
+                <h3>Hey there,</h3>
+                <h1>I'm Reece Lardy</h1>
+                <h3>Software <u>Developer</u> | Computer <u>Engineer</u></h3>
+                <br></br>
+                <p>I started my journey in the world of computers at young age,
+                and I am now 22 years old. I am a talented individual with strong communication skills, team spirit, and B.S. in Computer Engineering from the University of Wisconsin-Madison.
+                </p>
+                <br></br>
+                <p>I am in search of a full-time position as a Software Engineer. I bring expertise in writing full-stack code to support multiple
+                platforms, including web, android, and iOS, and a solid grasp of algorithms, databases, data structures,
+                and object-oriented designs. I have quality experience using industry-standard technologies, and am excited to join the workforce after college.
+            </p>
+
+            </div>
+        )
+    }
+}
+
+export default About
